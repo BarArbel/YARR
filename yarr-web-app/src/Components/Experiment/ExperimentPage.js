d978048095795e83a3dd7041fb367e420ab3ae93
bararbel@gmail.com <bararbel@gmail.com>
2020-03-21 10:52:34 +0200

    changed Dynamic to Adaptive

--- yarr-web-app/src/Components/Experiment/ExperimentPage.js
+++ yarr-web-app/src/Components/Experiment/ExperimentPage.js
@@ -58,7 +58,7 @@ class ExperimentPage extends Component {
   renderRounds() {
     const { Rounds } = this.props.experiment
     const gameMode = ["Cooperative", "Competitive"]
-    const difficulty = ["Dynamic", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6"]
+    const difficulty = ["Adaptive", "Level 1", "Level 2", "Level 3", "Level 4", "Level 5", "Level 6"]
 
     return (
       <div>
