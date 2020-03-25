acfd7f642618becf85e23c3a314d3125ab76e04f
Yuval Ozeri <Yuuval@gmail.com>
2020-03-25 23:29:28 +0200

    Ops, Uploaded wrong files last time

---
+++ YARR_Server/Classes/Table.js
@@ -0,0 +1,8 @@
+var shortID = require('shortid');
+
+module.exports = class Table {
+    constructor(){
+        this.time = Date.now();
+        this.id = shortID.generate();
+    }
+}
--- YARR_Server/index.js
+++ YARR_Server/index.js
@@ -1,139 +1,58 @@
 var io = require('socket.io')(process.env.PORT || 52300);
-
-//Custom Classes
-var Player = require('./Classes/Player.js');
-var Bullet = require('./Classes/Bullet.js');
+const mysqlConnection = require("./connection");
+var Table = require('./Classes/Table.js');
 
 console.log('Server has started');
 
-var players = [];
+var tables = [];
 var sockets = [];
-var bullets = [];
-
-//Updates
-setInterval(() => {
-    bullets.forEach(bullet => {
-        var isDestroyed = bullet.onUpdate();
-
-        //Remove
-        if(isDestroyed){
-            var index = bullets.indexOf(bullet);
-            if(index > -1){
-                bullets.splice(index, 1);
-
-                var returnData = {
-                    id: bullet.id
-                }
-
-                for(var playerID in players){
-                    sockets[playerID].emit('serverUnspawn',returnData);
-                }
-            }
-        }else{
-            var returnData = {
-                id: bullet.id,
-                position: {
-                    x: bullet.position.x,
-                    y: bullet.position.y
-                }
-            }
-
-            for(var playerID in players){
-                sockets[playerID].emit('updatePosition',returnData);
-            }
-        }
-    });
-}, 100,0);
 
 io.on('connection',function(socket){
     console.log('Connection Made!');
 
-    var player = new Player();
-    var thisPlayerID = player.id;
+    var table = new Table();
+    var thisTableID = table.id;
 
-    players[thisPlayerID] = player;
-    sockets[thisPlayerID] = socket;
+    tables[thisTableID] = table;
+    sockets[thisTableID] = socket;
+
+    tables.push();
+    socket.emit('ExperimentID',table);
+
+    //Creating table for each experiment
+    var sql = `CREATE TABLE yarrserver.ExperimentID_${table.time}_${table.id} (
+        EventID int unsigned NOT NULL AUTO_INCREMENT,
+        Timestamp float NOT NULL,
+        Event enum('pickup','giveItem','revivePlayer','temporaryLose','revived','lose','dropItem','getDamaged','blockDamage','failPickup','fallAccidently','individualLose','spawn') NOT NULL,
+        PlayerID int unsigned DEFAULT NULL,
+        CoordX float DEFAULT NULL,
+        CoordY float DEFAULT NULL,
+        Item int DEFAULT NULL,
+        Enemy int DEFAULT NULL,
+        GameMode enum('cooperative','competitive') NOT NULL,
+        PRIMARY KEY (EventID)
+    );`;
+
+    mysqlConnection.query(sql,(err,rows,fields) => {
+        if(err) throw err;
+        console.log("data was added");
+    })
 
 
-    //Tell the client that this is our id for the server
-    socket.emit('register', {id: thisPlayerID});
-    socket.emit('spawn', player); //Tell myself i have spawned
-    socket.broadcast.emit('spawn', player) //Tell others I have spawned
-
-    //Tell myself about everyone else in the game
-    for (var playerID in players) {
-        if (playerID != thisPlayerID) {
-            socket.emit('spawn', players[playerID]);
-        }
-    }
-
-
-    //Positional Data from Client
-    socket.on('updatePosition', function(data){
-        player.position.x  = data.position.x;
-        player.position.y = data.position.y;
-
-        socket.broadcast.emit('updatePosition',player);
-    });
-
-    socket.on('updateRotation', function(data){
-        player.tankRotation = data.tankRotation;
-        player.barrelRotation = data.barrelRotation;
-
-        socket.broadcast.emit('updateRotation',player);
-    });
-
-    socket.on('fireBullet',function(data){
-        var bullet = new Bullet();
-        bullet.name = 'Bullet';
-        bullet.position.x = data.position.x;
-        bullet.position.y = data.position.y;
-        bullet.direction.x = data.direction.x;
-        bullet.direction.y = data.direction.y;
-
-        bullets.push(bullet);
-
-        var returnData = {
-            name: bullet.name,
-            id: bullet.id,
-            position: {
-                x:bullet.position.x,
-                y:bullet.position.y
-            },
-            direction: {
-                x:bullet.direction.x,
-                y:bullet.direction.y
-            }
-        }
-
-        socket.emit('serverSpawn', returnData);
-        socket.broadcast.emit('serverSpawn', returnData);
+    //Sending Data to the spesific table
+    socket.on('gameSnapshot', function(data){
+        var sql = `INSERT INTO yarrserver.ExperimentID_${table.time}_${table.id}(Timestamp,Event,PlayerID,CoordX,CoordY,Item,Enemy,GameMode) 
+                                              VALUES('${data.Time}','${data.Event}','${data.PlayerID}','${data.CoordX}','${data.CoordY}','${data.Item}','${data.Enemy}','${data.GameMode}');`;
+        mysqlConnection.query(sql,(err,rows,fields) => {
+            if(err) throw err;
+            console.log("data was added");
+        })
     });
 
     socket.on('disconnect', function(){
         console.log('A player has disconnected');
-        delete players[thisPlayerID];
-        delete sockets[thisPlayerID];
-        socket.broadcast.emit('disconected',player);
+        delete tables[thisTableID];
+        delete sockets[thisTableID];
     });
 });
-
-
-function interval(func,wait,times){
-    var interv = function(w,t){
-        return function(){
-            if(typeof t === "undefined" || t-- > 0){
-                setTimeout(interv,w)
-                try{
-                    func.call(null);
-                }catch(e){
-                    t = 0;
-                    throw e.toString();
-                }
-            }
-        };
-    }(wait,times)
-
-    setTimeout(interv,wait);
-}
 
