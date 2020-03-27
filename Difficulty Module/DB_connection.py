4ac23995f8ad7a03b40ae3277c5d5758f1d80210
bararbel@gmail.com <bararbel@gmail.com>
2020-03-27 16:50:07 +0300

    difficulty module progress

--- Difficulty Module/DB_connection.py
+++ Difficulty Module/DB_connection.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class DB_connection:
 
-    def __init__(self, table_name):
+    def __init__(self, table_name, numberOfPlayers):
         self.db = 'yarr_game1'
         self.tb = table_name
         self.DDAtb = "dda_"+table_name
@@ -17,26 +17,58 @@ class DB_connection:
                                            database=self.db)
         self.cursor = self.cnx.cursor()
         self.create_DDA_table(self)
+        self.initialize_DDA_table(self, numberOfPlayers)
 
     def create_DDA_table(self):
         query = ("CREATE TABLE `" + self.db + "`.`" + self.DDAtb + "` ("
                  "`Index` INT UNSIGNED NOT NULL AUTO_INCREMENT,"
                  "`PlayerID` INT UNSIGNED NOT NULL,"
                  "`Threshold` FLOAT NOT NULL,"
-                 "`I_SpawnHeight_level` INT UNSIGNED NOT NULL,"
-                 "`I_SpawnHeight_skill` BOOLEAN NOT NULL,"
-                 "`I_DestroyTimer_level` INT UNSIGNED NOT NULL,"
-                 "`I_DestroyTimer_skill` BOOLEAN NOT NULL,"
-                 "`E_Precision_level` INT UNSIGNED NOT NULL,"
-                 "`E_Precision_skill` BOOLEAN NOT NULL,"
-                 "`E_Speed_level` INT UNSIGNED NOT NULL,"
-                 "`E_Speed_skill` BOOLEAN NOT NULL,"
-                 "`E_SpawnRate_level` INT UNSIGNED NOT NULL,"
-                 "`E_SpawnRate_skill` BOOLEAN NOT NULL,"
+                 "`I_SpawnHeight_level` INT NOT NULL,"
+                 "`I_SpawnHeight_skill` FLOAT NOT NULL,"
+                 "`I_DestroyTimer_level` INT NOT NULL,"
+                 "`I_DestroyTimer_skill` FLOAT NOT NULL,"
+                 "`E_Precision_level` INT NOT NULL,"
+                 "`E_Precision_skill` FLOAT NOT NULL,"
+                 "`E_Speed_level` INT NOT NULL,"
+                 "`E_Speed_skill` FLOAT NOT NULL,"
+                 "`E_SpawnRate_level` INT NOT NULL,"
+                 "`E_SpawnRate_skill` FLOAT NOT NULL,"
                  "PRIMARY KEY (`Index`))"
                  "DEFAULT CHARACTER SET = utf8;")
 
         self.cursor.execute(query)
+
+    def initialize_DDA_table(self, numberOfPlayers):
+
+        initial_SpawnHeight_skill = "3"
+        initial_DestroyTimer_skill = "3"
+        initial_Precision_skill = "2"
+        initial_Speed_skill = "2"
+        initial_SpawnRate_skill = "2"
+
+        for i in range(numberOfPlayers):
+            query = ("INSERT INTO `" + self.db + "`.`" + self.DDAtb + "` (" +
+                     "PlayerID, Threshold, I_SpawnHeight_level, " +
+                     "I_SpawnHeight_skill, I_DestroyTimer_level, " +
+                     "I_DestroyTimer_skill, E_Precision_level, " +
+                     "E_Precision_skill, E_Speed_level, E_Speed_skill, " +
+                     "E_SpawnRate_level, E_SpawnRate_skill)" +
+                     "VALUES (" + str(i + 1) + ", 0, 0, " +
+                     initial_SpawnHeight_skill + ", 0, " +
+                     initial_DestroyTimer_skill + ", 0, " +
+                     initial_Precision_skill + ", 0, " + initial_Speed_skill +
+                     ", 0, " + initial_SpawnRate_skill + ")")
+
+            self.cursor.execute(query)
+            self.cnx.commit()
+
+    def get_DDA_last_player_skill(self, skill, player_id):
+        query = ("SELECT " + skill + " WHERE PlayerID = " + str(player_id) +
+                 "FROM `" + self.db + "`.`" + self.DDAtb +
+                 "` ORDER BY Index ASC LIMIT 1")
+        self.cursor.execute(query)
+        return self.cursor.fetchone()
 
     def remove_DDA_table(self):
         query = ("DROP TABLE `" + self.db + "`.`" + self.DDAtb + "`")
@@ -49,13 +81,16 @@ class DB_connection:
 
     def count_total_player_events(self, event, player_id):
         query = ("SELECT count(" + event + ") FROM `" + self.tb +
-                 "` WHERE Event = " + event + " AND PlayerID = " + player_id)
+                 "` WHERE Event = " + event + " AND PlayerID = " +
+                 str(player_id))
         self.cursor.execute(query)
+        return self.cursor.fetchall()
 
     def count_total_team_events(self, value, event):
         query = ("SELECT count(" + value + ") FROM `" + self.tb +
                  "` WHERE Event = " + event)
         self.cursor.execute(query)
+        return self.cursor.fetchall()
 
     def insert_DDA_table(self, PlayerID, Threshold, I_SpawnHeight_level,
                          I_SpawnHeight_skill, I_DestroyTimer_level,
@@ -69,10 +104,13 @@ class DB_connection:
                  "I_DestroyTimer_skill, E_Precision_level, " +
                  "E_Precision_skill, E_Speed_level, E_Speed_skill, " +
                  "E_SpawnRate_level, E_SpawnRate_skill)" +
-                 "VALUES (" + PlayerID + ", " + Threshold + ", " +
-                 I_SpawnHeight_level + ", " + I_SpawnHeight_skill + ", " +
-                 I_DestroyTimer_level + ", " + I_DestroyTimer_skill + ", " +
-                 E_Precision_level + ", " + E_Precision_skill + ", " +
-                 E_Speed_level + ", " + E_Speed_skill + ", " +
-                 E_SpawnRate_level + ", " + E_SpawnRate_skill + ")")
+                 "VALUES (" + str(PlayerID) + ", " + str(Threshold) + ", " +
+                 str(I_SpawnHeight_level) + ", " + str(I_SpawnHeight_skill) +
+                 ", " + str(I_DestroyTimer_level) + ", " +
+                 str(I_DestroyTimer_skill) + ", " + str(E_Precision_level) +
+                 ", " + str(E_Precision_skill) + ", " + str(E_Speed_level) +
+                 ", " + str(E_Speed_skill) + ", " + str(E_SpawnRate_level) +
+                 ", " + str(E_SpawnRate_skill) + ")")
+
         self.cursor.execute(query)
+        self.cnx.commit()
--- Difficulty Module/__init__.py
+++ Difficulty Module/__init__.py
@@ -21,54 +21,112 @@ def getDataFromDB(con, numberOfPlayers):
         "spawn": []
     }
 
+    lastSkills = {
+        "I_SpawnHeight_skill": [],
+        "E_Precision_skill": [],
+        "E_Speed_skill": []
+    }
+
     for player_id in range(numberOfPlayers):
         for event in total:
-            total[event].append(con.count_total_player_events(event,
-                                                              player_id))
-    return total
+            total[event].append(con.count_total_player_events(
+                event, player_id + 1))
+
+        for skill in lastSkills:
+            lastSkills[skill].append(con.get_DDA_last_player_skill(
+                skill, player_id + 1))
+
+    return total, lastSkills
 
 
-def calculate(numberOfPlayers, total):
+def calculate(numberOfPlayers, total, lastSkills):
 
     calcs = {
         "threshold": 0,
-        "spawnHeightAndTimer": [],
-        "precision": [],
-        "speedAndSpawnRate": []
+        "spawnHeigthAndTimer": {
+            "skill": [],
+            "level": []
+        },
+        "precision": {
+            "skill": [],
+            "level": []
+        },
+        "speedAndSpawnRate": {
+            "skill": [],
+            "level": []
+        }
     }
+
+    key_pairs = [
+        ["spawnHeigthAndTimer", "I_SpawnHeight_skill"],
+        ["precision", "E_Precision_skill"],
+        ["speedAndSpawnRate", "E_Speed_skill"]
+    ]
 
     calc = DDA_calc()
 
     calcs["threshold"] = calc.calc_threshold(total["pickup"], total["spawn"])
 
     for player_id in range(numberOfPlayers):
-        calcs["spawnHeightAndTimer"].append(calc.calc_spawn_height_and_timer(
-            sum(total["pickup"]), total["fallAccidently"][player_id],
-            total["failPickup"][player_id], total["pickup"][player_id],
-            total["spawn"][player_id]))
+        calcs["spawnHeightAndTimer"]["skill"].append(
+            calc.calc_spawn_height_and_timer(
+                sum(total["pickup"]), total["fallAccidently"][player_id],
+                total["failPickup"][player_id], total["pickup"][player_id],
+                total["spawn"][player_id]
+            )
+        )
 
-        calcs["precision"].append(calc.calc_precision(
-            total["avoidDamage"][player_id], total["getDamaged"][player_id],
-            total["pickup"][player_id], total["spawn"][player_id]))
+        calcs["precision"]["skill"].append(
+            calc.calc_precision(
+                total["avoidDamage"][player_id],
+                total["getDamaged"][player_id], total["pickup"][player_id],
+                total["spawn"][player_id]
+            )
+        )
 
-        calcs["speedAndSpawnRate"].append(calc.calc_speed_and_spawn_rate(
-            total["avoidDamage"][player_id], total["getDamaged"][player_id],
-            total["blockDamage"][player_id], total["pickup"][player_id],
-            total["spawn"][player_id]))
+        calcs["speedAndSpawnRate"]["skill"].append(
+            calc.calc_speed_and_spawn_rate(
+                total["avoidDamage"][player_id],
+                total["getDamaged"][player_id],
+                total["blockDamage"][player_id], total["pickup"][player_id],
+                total["spawn"][player_id]
+            )
+        )
+
+        for pair in key_pairs:
+            rangeMax = lastSkills[pair[1]] + calcs["threshold"]
+            rangeMin = lastSkills[pair[1]] - calcs["threshold"]
+            skill = calcs[pair[0]]["skill"][-1]
+
+            calcs[pair[0]]["level"].append(1 if skill > rangeMax else (
+                -1 if skill <= rangeMin else 0))
 
     return calcs
 
 
 def insertCalculationsToDB(con, calcs):
+
+    for player_id in range(numberOfPlayers):
+        con.insert_DDA_table(player_id + 1, calcs["threshold"],
+                             calcs["spawnHeightAndTimer"]["level"][player_id],
+                             calcs["spawnHeightAndTimer"]["skill"][player_id],
+                             calcs["spawnHeightAndTimer"]["level"][player_id],
+                             calcs["spawnHeightAndTimer"]["skill"][player_id],
+                             calcs["precision"]["level"][player_id],
+                             calcs["precision"]["skill"][player_id],
+                             calcs["speedAndSpawnRate"]["level"][player_id],
+                             calcs["speedAndSpawnRate"]["skill"][player_id],
+                             calcs["speedAndSpawnRate"]["level"][player_id],
+                             calcs["speedAndSpawnRate"]["skill"][player_id])
     return
 
 
 if __name__ == '__main__':
 
     numberOfPlayers = 3
-    con = DB_connection("game_snapshot")
+    con = DB_connection("game_snapshot", numberOfPlayers)
     con.create_DDA_table()
 
-    total = getDataFromDB(con, numberOfPlayers)
-    calcs = calculate(numberOfPlayers, total)
+    total, lastSkills = getDataFromDB(con, numberOfPlayers)
+    calcs = calculate(numberOfPlayers, total, lastSkills)
     insertCalculationsToDB(con, calcs)
