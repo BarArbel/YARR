ea7d137cdccef50da1cab5b0c68450e1eaeff716
bararbel@gmail.com <bararbel@gmail.com>
2020-04-07 10:27:19 +0300

    moved calc to global

--- Difficulty Module/__init__.py
+++ Difficulty Module/__init__.py
@@ -10,6 +10,7 @@ sio = socketio.AsyncClient()
 first_connection = True
 table_name = ""
 con = None
+calc = DDA_calc()
 number_of_players = 3
 host = os.getenv('HOST')
 recv_port = os.getenv('PORT')
@@ -75,8 +76,6 @@ async def calculate(total, last_skills):
         ["precision", "E_Precision_skill"],
         ["speedAndSpawnRate", "E_Speed_skill"]
     ]
-
-    calc = DDA_calc()
 
     calcs["threshold"] = calc.calc_threshold(total["pickup"], total["spawn"])
 
