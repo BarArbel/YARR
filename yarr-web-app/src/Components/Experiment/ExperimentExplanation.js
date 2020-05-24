fc81cddd9116ec641dca0b776d7919b3c864efcb
GoldenDude <edanazran@gmail.com>
2020-05-24 19:56:38 +0300

    finished SVG hell > Edan

--- yarr-web-app/src/Components/Experiment/ExperimentExplanation.js
+++ yarr-web-app/src/Components/Experiment/ExperimentExplanation.js
@@ -1,5 +1,22 @@
 import React, { Component } from "react";
 import Tooltip from '@material-ui/core/Tooltip';
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
+
+const defaultTheme = createMuiTheme();
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiTooltip: {
+      tooltip: {
+        fontSize: "14px",
+        maxWidth: 'none',
+      }
+    }
+  }
+})
 
 class ExperimentExplanation extends Component {
 
@@ -21,83 +38,93 @@ class ExperimentExplanation extends Component {
     const { onHover } = this.state
 
     return (
-      <div className="experimentExplanation">
-        <svg
-          viewBox="0 0 1005.3333 740"
-          height="100%"
-          width="100%"
-        >
-          <image style={{width: "100%", height: "100%"}} href={require("../../Images/Game.png")}/>
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="1005" height="38" y="87"/>
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="42" height="125" y="125" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="95" height="402" y="250" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="950" height="20" x="95" y="250" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="140" height="278" x="95" y="375" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="900" height="125" x="142" y="125" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="75" height="383" x="235" y="270" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="695" height="32" x="310" y="270" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="25" height="87" x="415" y="467" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="565" height="251" x="440" y="302" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="430" height="100" x="595" y="553" />
-          <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="105" height="186" x="310" y="467" />
+      <MuiThemeProvider theme={defaultTheme}>
+        <div className="experimentExplanation">
+          <svg
+            viewBox="0 0 1005.3333 740"
+            height="100%"
+            width="100%"
+          >
+            <image style={{width: "100%", height: "100%"}} href={require("../../Images/Game.png")}/>
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="1005" height="38" y="87"/>
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="42" height="125" y="125" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="95" height="402" y="250" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="950" height="20" x="95" y="250" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="140" height="278" x="95" y="375" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="900" height="125" x="142" y="125" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="75" height="383" x="235" y="270" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="695" height="32" x="310" y="270" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="25" height="87" x="415" y="467" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="565" height="251" x="440" y="302" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="430" height="100" x="595" y="553" />
+            <rect className={`svgBlock ${onHover !== -1 ? "nonFocus" : null}`} width="105" height="186" x="310" y="467" />
 
-          <Tooltip title={"Parrot - The red character has to avoid its enemy "}>
-            <rect 
-              onMouseOver={() => this.handleOnHover(0)} 
-              onMouseLeave={() => this.handleOnHover(-1)} 
-              className={`svgBlock ${onHover !== -1 && onHover !== 0 ? "nonFocus" : null}`}
-              stroke="#ffae42" 
-              strokeWidth={onHover === -1 ? "2" : "0"} 
-              width="100" 
-              height="125" 
-              x="42" 
-              y="125"
-            />
-          </Tooltip>
+            <MuiThemeProvider theme={theme}>
+              <Tooltip title={"Parrot - The red character has to avoid its enemy "}>
+                <rect 
+                  onMouseOver={() => this.handleOnHover(0)} 
+                  onMouseLeave={() => this.handleOnHover(-1)} 
+                  className={`svgBlock ${onHover !== -1 && onHover !== 0 ? "nonFocus" : null}`}
+                  stroke="#ffae42" 
+                  strokeWidth={onHover === -1 ? "2" : "0"} 
+                  width="100" 
+                  height="125" 
+                  x="42" 
+                  y="125"
+                />
+              </Tooltip>
+            </MuiThemeProvider>
 
-          <Tooltip title={"Treasure - The red character has to pick it up"}>
-            <rect 
-              onMouseOver={() => this.handleOnHover(1)}
-              onMouseLeave={() => this.handleOnHover(-1)} 
-              className={`svgBlock ${onHover !== -1 && onHover !== 1 ? "nonFocus" : null}`}
-              stroke="#ffae42" 
-              strokeWidth={onHover === -1 ? "2" : "0"} 
-              width="140" 
-              height="105" 
-              x="95" 
-              y="270" 
-            />
-          </Tooltip>
+            <MuiThemeProvider theme={theme}>
+              <Tooltip title={"Treasure - The red character has to pick it up"}>
+                <rect 
+                  onMouseOver={() => this.handleOnHover(1)}
+                  onMouseLeave={() => this.handleOnHover(-1)} 
+                  className={`svgBlock ${onHover !== -1 && onHover !== 1 ? "nonFocus" : null}`}
+                  stroke="#ffae42" 
+                  strokeWidth={onHover === -1 ? "2" : "0"} 
+                  width="140" 
+                  height="105" 
+                  x="95" 
+                  y="270" 
+                />
+              </Tooltip>
+            </MuiThemeProvider>
 
-          <Tooltip title={"Character - Controlled by a player"}>
-            <rect 
-              onMouseOver={() => this.handleOnHover(2)}
-              onMouseLeave={() => this.handleOnHover(-1)} 
-              className={`svgBlock ${onHover !== -1 && onHover !== 2 ? "nonFocus" : null}`}
-              stroke="#ffae42" 
-              strokeWidth={onHover === -1 ? "2" : "0"} 
-              width="130" 
-              height="165" 
-              x="310" 
-              y="302" 
-            />
-          </Tooltip>
+            <MuiThemeProvider theme={theme}>
+              <Tooltip title={"Character - Controlled by a player"}>
+                <rect 
+                  onMouseOver={() => this.handleOnHover(2)}
+                  onMouseLeave={() => this.handleOnHover(-1)} 
+                  className={`svgBlock ${onHover !== -1 && onHover !== 2 ? "nonFocus" : null}`}
+                  stroke="#ffae42" 
+                  strokeWidth={onHover === -1 ? "2" : "0"} 
+                  width="130" 
+                  height="165" 
+                  x="310" 
+                  y="302" 
+                />
+              </Tooltip>
+            </MuiThemeProvider>
 
-          <Tooltip title={"Pile - The characters' goal is to put their treasures here"}>
-            <rect 
-              onMouseOver={() => this.handleOnHover(3)}
-              onMouseLeave={() => this.handleOnHover(-1)} 
-              className={`svgBlock ${onHover !== -1 && onHover !== 3 ? "nonFocus" : null}`}
-              stroke="#ffae42" 
-              strokeWidth={onHover === -1 ? "2" : "0"} 
-              width="180" 
-              height="100" 
-              x="415"
-              y="553" 
-            />
-          </Tooltip>
-        </svg>
-      </div>
+            <MuiThemeProvider theme={theme}>
+              <Tooltip title={"Pile - The characters' goal is to put their treasures here"}>
+                <rect 
+                  onMouseOver={() => this.handleOnHover(3)}
+                  onMouseLeave={() => this.handleOnHover(-1)} 
+                  className={`svgBlock ${onHover !== -1 && onHover !== 3 ? "nonFocus" : null}`}
+                  stroke="#ffae42" 
+                  strokeWidth={onHover === -1 ? "2" : "0"} 
+                  width="180" 
+                  height="100" 
+                  x="415"
+                  y="553" 
+                />
+              </Tooltip>
+            </MuiThemeProvider>
+          </svg>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
