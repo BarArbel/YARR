f6fc11724b6b3711b8918f11887301b7fb43ad41
MiriNu <miryamnudelman@gmail.com>
2020-05-29 14:47:26 +0300

    sqlstuff

--- Backend Services/Insights Service/controllers/DataAnalysisController.js
+++ Backend Services/Insights Service/controllers/DataAnalysisController.js
@@ -1,8 +1,52 @@
 const { query } = require('../database.js');
 
-async function QueryA(){
+async function QueryExpBar(experimentId){
   try {
-    let results = query(`insertQueryHereSQLQueenAverageDickCarryGoMidz`);
+    let sql_percents = `
+    INSERT INTO yarr.exp_insights_bar
+    select * from (
+        select  ExperimentID, GameMode, 
+                (sum(ItemTaken)/sum(ItemSpawns))*100 PercentItemsTaken,
+                100-((sum(ItemTaken)/sum(ItemSpawns))*100) PercentItemsMissed,
+                (sum(EnemyAvoid)/sum(EnemySpawns)*100) PercentEnemiesAvoid,
+                (sum(EnemyDamage)/sum(EnemySpawns)*100) PercentEnemiesHit,
+                (sum(EnemyBlock)/sum(EnemySpawns)*100) PercentEnemiesBlock
+        from
+            (select if(Event = 'spawn' and Item!=0, 1,0) ItemSpawns, 
+                    if(Event = 'pickup' and Item!=0,1,0) ItemTaken,
+                    if(Event = 'spawn' and Enemy!=0, 1,0) EnemySpawns, 
+                    if(Event = 'avoidDamage' and Enemy!=0, 1,0) EnemyAvoid,
+                    if(Event = 'blockDamage' and Enemy!=0, 1,0) EnemyBlock,
+                    if(Event = 'getDamaged' and Enemy!=0, 1,0) EnemyDamage,
+                    ExperimentID, GameMode
+            from raw_data 
+            where ExperimentID = ${experimentId} and GameMode = 'Cooperative') as counter
+        group by 1,2    ) coop_sums 
+    union all (
+    select ExperimentID, GameMode, 
+          (sum(ItemTaken)/sum(ItemSpawns))*100 PercentItemsTaken,
+          100-((sum(ItemTaken)/sum(ItemSpawns))*100) PercentItemsMissed,
+          (sum(EnemyAvoid)/sum(EnemySpawns)*100) PercentEnemiesAvoid,
+          (sum(EnemyDamage)/sum(EnemySpawns)*100) PercentEnemiesHit,
+          (sum(EnemyBlock)/sum(EnemySpawns)*100) PercentEnemiesBlock
+    from
+        (select if(Event = 'spawn' and PlayerID!=0, 1,0) ItemSpawns, 
+          if(Event = 'pickup' and PlayerID!=0,1,0) ItemTaken,
+          if(Event = 'spawn' and PlayerID!=0, 1,0) EnemySpawns, 
+          if(Event = 'avoidDamage' and PlayerID!=0, 1,0) EnemyAvoid,
+          if(Event = 'blockDamage' and PlayerID!=0, 1,0) EnemyBlock,
+          if(Event = 'getDamaged' and PlayerID!=0, 1,0) EnemyDamage,
+                    ExperimentID, GameMode
+        from raw_data 
+        where ExperimentID = ${experimentId} and GameMode = 'Competitive') as counter
+        group by 1,2    )
+    ON DUPLICATE KEY UPDATE Mode = VALUES(Mode), 
+							              PercentItemsTaken = VALUES(PercentItemsTaken), 
+                            PercentItemsMissed= VALUES(PercentItemsMissed),
+                            PercentEnemiesAvoid = VALUES(PercentEnemiesAvoid),
+                            PercentEnemiesHit = VALUES(PercentEnemiesHit),
+                            PercentEnemiesBlock = VALUES(PercentEnemiesBlock);`;
+    let results = query(sql_percents);
 
     // do stuff with results
     //insert query should be here
@@ -49,7 +93,7 @@ module.exports = {
       return;
     }
 
-    let result = await QueryA();
+    let result = await QueryExpBar(experimentId);
     // can check if result's good here
     result = await QueryB();
 
